8eef09713d6ad61a05e07b2c2edd37d14e0391da
frodikarlsson <fr8125ka-s@student.lu.se>
2021-04-11 15:31:52 +0200

    added core to each member folder

---
+++ src/amoebas/competition2021/andre/core.clj
@@ -0,0 +1,3 @@
+(ns amoebas.competition2021.andre.core
+    (:use amoebas.defs amoebas.lib amoebas.run)
+    )
---
+++ src/amoebas/competition2021/frodi/core.clj
@@ -0,0 +1,3 @@
+(ns amoebas.competition2021.frodi.core
+    (:use amoebas.defs amoebas.lib amoebas.run)
+    )
